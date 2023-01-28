1d9c0f2ea10479016dc11fc77fc331c462483a16
hamedyousefian <hamed77yosefian@gmail.com>
2023-01-28 16:13:45 +0330

    Update New Version

--- src/ckeditor.js
+++ src/ckeditor.js
@@ -10,12 +10,11 @@ import AutoLink from '@ckeditor/ckeditor5-link/src/autolink.js';
 import Autosave from '@ckeditor/ckeditor5-autosave/src/autosave.js';
 import BlockQuote from '@ckeditor/ckeditor5-block-quote/src/blockquote.js';
 import Bold from '@ckeditor/ckeditor5-basic-styles/src/bold.js';
-import CloudServices from '@ckeditor/ckeditor5-cloud-services/src/cloudservices.js';
 import Code from '@ckeditor/ckeditor5-basic-styles/src/code.js';
 import CodeBlock from '@ckeditor/ckeditor5-code-block/src/codeblock.js';
+import DataFilter from '@ckeditor/ckeditor5-html-support/src/datafilter.js';
+import DataSchema from '@ckeditor/ckeditor5-html-support/src/dataschema.js';
 import Essentials from '@ckeditor/ckeditor5-essentials/src/essentials.js';
-import ExportPdf from '@ckeditor/ckeditor5-export-pdf/src/exportpdf.js';
-import ExportWord from '@ckeditor/ckeditor5-export-word/src/exportword.js';
 import FindAndReplace from '@ckeditor/ckeditor5-find-and-replace/src/findandreplace.js';
 import FontBackgroundColor from '@ckeditor/ckeditor5-font/src/fontbackgroundcolor.js';
 import FontColor from '@ckeditor/ckeditor5-font/src/fontcolor.js';
@@ -50,6 +49,7 @@ import PageBreak from '@ckeditor/ckeditor5-page-break/src/pagebreak.js';
 import Paragraph from '@ckeditor/ckeditor5-paragraph/src/paragraph.js';
 import PasteFromOffice from '@ckeditor/ckeditor5-paste-from-office/src/pastefromoffice.js';
 import RemoveFormat from '@ckeditor/ckeditor5-remove-format/src/removeformat.js';
+import SimpleUploadAdapter from '@ckeditor/ckeditor5-upload/src/adapters/simpleuploadadapter.js';
 import SourceEditing from '@ckeditor/ckeditor5-source-editing/src/sourceediting.js';
 import SpecialCharacters from '@ckeditor/ckeditor5-special-characters/src/specialcharacters.js';
 import SpecialCharactersArrows from '@ckeditor/ckeditor5-special-characters/src/specialcharactersarrows.js';
@@ -69,9 +69,7 @@ import TableCellProperties from '@ckeditor/ckeditor5-table/src/tablecellproperti
 import TableColumnResize from '@ckeditor/ckeditor5-table/src/tablecolumnresize.js';
 import TableProperties from '@ckeditor/ckeditor5-table/src/tableproperties';
 import TableToolbar from '@ckeditor/ckeditor5-table/src/tabletoolbar.js';
-import TextPartLanguage from '@ckeditor/ckeditor5-language/src/textpartlanguage.js';
 import TextTransformation from '@ckeditor/ckeditor5-typing/src/texttransformation.js';
-import Title from '@ckeditor/ckeditor5-heading/src/title.js';
 import TodoList from '@ckeditor/ckeditor5-list/src/todolist';
 import Underline from '@ckeditor/ckeditor5-basic-styles/src/underline.js';
 import WordCount from '@ckeditor/ckeditor5-word-count/src/wordcount.js';
@@ -87,12 +85,11 @@ Editor.builtinPlugins = [
 	Autosave,
 	BlockQuote,
 	Bold,
-	CloudServices,
 	Code,
 	CodeBlock,
+	DataFilter,
+	DataSchema,
 	Essentials,
-	ExportPdf,
-	ExportWord,
 	FindAndReplace,
 	FontBackgroundColor,
 	FontColor,
@@ -127,6 +124,7 @@ Editor.builtinPlugins = [
 	Paragraph,
 	PasteFromOffice,
 	RemoveFormat,
+	SimpleUploadAdapter,
 	SourceEditing,
 	SpecialCharacters,
 	SpecialCharactersArrows,
@@ -146,9 +144,7 @@ Editor.builtinPlugins = [
 	TableColumnResize,
 	TableProperties,
 	TableToolbar,
-	TextPartLanguage,
 	TextTransformation,
-	Title,
 	TodoList,
 	Underline,
 	WordCount
@@ -160,49 +156,57 @@ Editor.defaultConfig = {
 		items: [
 			'heading',
 			'|',
-			'bold',
-			'italic',
-			'link',
-			'bulletedList',
-			'numberedList',
-			'|',
-			'outdent',
-			'indent',
-			'|',
-			'imageUpload',
-			'blockQuote',
-			'insertTable',
-			'mediaEmbed',
-			'undo',
-			'redo',
 			'alignment',
-			'codeBlock',
-			'exportWord',
-			'exportPdf',
-			'findAndReplace',
+			'indent',
+			'outdent',
+			'|',
+			'MathType',
+			'ChemType',
+			'|',
+			'blockQuote',
+			'bold',
+			'underline',
+			'italic',
+			'strikethrough',
+			'|',
 			'fontBackgroundColor',
 			'fontColor',
 			'fontFamily',
 			'fontSize',
 			'highlight',
-			'horizontalLine',
-			'htmlEmbed',
-			'MathType',
-			'ChemType',
-			'pageBreak',
-			'removeFormat',
-			'sourceEditing',
-			'imageInsert',
-			'code',
+			'|',
 			'todoList',
-			'underline',
-			'textPartLanguage',
+			'numberedList',
+			'bulletedList',
+			'|',
+			'imageInsert',
+			'mediaEmbed',
+			'imageUpload',
+			'|',
 			'style',
-			'subscript',
+			'|',
+			'insertTable',
+			'|',
+			'undo',
+			'redo',
+			'|',
+			'sourceEditing',
+			'|',
 			'superscript',
-			'strikethrough',
-			'restrictedEditingException',
-			'specialCharacters'
+			'subscript',
+			'specialCharacters',
+			'removeFormat',
+			'|',
+			'link',
+			'|',
+			'code',
+			'codeBlock',
+			'htmlEmbed',
+			'|',
+			'findAndReplace',
+			'horizontalLine',
+			'pageBreak',
+			'|'
 		]
 	},
 	language: 'fa',
